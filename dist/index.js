053985ccf4d9b29b534f182b962d135ac8d928bb
Maxi <mmaximonchuk@gmail.com>
2023-04-18 18:49:00 +0300

    fix: added env port

--- dist/index.js
+++ dist/index.js
@@ -5,7 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.get("/", (req, res) => {
     res.send("Welcome to backend!");
 });
