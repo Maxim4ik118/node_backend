be7fe58d7b6ccaadc0183a8db8228d0ba04165bb
Maxi <mmaximonchuk@gmail.com>
2023-04-18 18:03:49 +0300

    first commit

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import express, { Request, Response } from "express";
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Welcome to backend!");
+});
+
+app.listen(port, () => console.log("Server running on port " + port));
