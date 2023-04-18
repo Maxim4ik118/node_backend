eb1f41bdb952d62d8f71a0eab8f0da42264877f6
Maxi <mmaximonchuk@gmail.com>
2023-04-18 18:31:34 +0300

    check

--- dist/index.js
+++ dist/index.js
@@ -9,4 +9,4 @@ const port = 3000;
 app.get("/", (req, res) => {
     res.send("Welcome to backend!");
 });
-app.listen(port, () => console.log("Server running on port " + port));
+app.listen(port, () => console.log("Server running on port: " + port));
--- src/index.ts
+++ src/index.ts
@@ -7,4 +7,4 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Welcome to backend!");
 });
 
-app.listen(port, () => console.log("Server running on port " + port));
+app.listen(port, () => console.log("Server running on port: " + port));
