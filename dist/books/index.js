0d6f7a83fa4c08104325a8d59cadc24b1d613e8b
Maxi <mmaximonchuk@gmail.com>
2023-04-20 13:07:06 +0300

    updated: fix

---
+++ dist/books/index.js
@@ -0,0 +1,39 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.add = exports.getById = exports.getAll = void 0;
+// import { nanoid } from "nanoid";
+const promises_1 = __importDefault(require("fs/promises"));
+const path_1 = __importDefault(require("path"));
+const { nanoid } = require("nanoid");
+const booksPath = path_1.default.join(__dirname, "./books.json");
+const getAll = () => __awaiter(void 0, void 0, void 0, function* () {
+    const data = yield promises_1.default.readFile(booksPath, "utf-8");
+    return JSON.parse(data);
+});
+exports.getAll = getAll;
+const getById = (id) => __awaiter(void 0, void 0, void 0, function* () {
+    const books = yield getAll();
+    const book = books.find((book) => book.id === id);
+    return book || null;
+});
+exports.getById = getById;
+const add = (bookData) => __awaiter(void 0, void 0, void 0, function* () {
+    const newBook = Object.assign({ id: nanoid() }, bookData);
+    const books = yield getAll();
+    const updatedBooks = [...books, newBook];
+    yield promises_1.default.writeFile(booksPath, JSON.stringify(updatedBooks));
+    return newBook;
+});
+exports.add = add;
